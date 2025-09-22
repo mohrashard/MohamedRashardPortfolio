16b6e33d47ea08a2311abd04f68c438745aa27ad
mohrashard <mohrashard@gmail.com>
2025-09-22 15:23:12 +0530

    fixed

--- src/AnimatedRoutes.js
+++ src/AnimatedRoutes.js
@@ -1,11 +1,14 @@
-
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+
+import App from "./App";
 import Services from "./Services";
 import WebApp from "./WebApp";
 import MobileApps from "./MobileApp";
 import AIAndML from "./AISolution";
 import FrontendWeb from "./FrontendWeb";
+import Blog from "./Blog";
+import Post from "./Post";
 import PageWrapper from "./PageWrapper";
 
 export default function AnimatedRoutes() {
@@ -14,11 +17,34 @@ export default function AnimatedRoutes() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/services" element={<PageWrapper><Services /></PageWrapper>} />
-        <Route path="/services/web-apps" element={<PageWrapper><WebApp /></PageWrapper>} />
-        <Route path="/services/mobile-apps" element={<PageWrapper><MobileApps /></PageWrapper>} />
-        <Route path="/services/ai-solutions" element={<PageWrapper><AIAndML /></PageWrapper>} />
-        <Route path="/services/frontend-websites" element={<PageWrapper><FrontendWeb /></PageWrapper>} />
+        {/* Homepage */}
+        <Route path="/" element={<App />} />
+
+        {/* Services */}
+        <Route
+          path="/services"
+          element={<PageWrapper><Services /></PageWrapper>}
+        />
+        <Route
+          path="/services/web-apps"
+          element={<PageWrapper><WebApp /></PageWrapper>}
+        />
+        <Route
+          path="/services/mobile-apps"
+          element={<PageWrapper><MobileApps /></PageWrapper>}
+        />
+        <Route
+          path="/services/ai-solutions"
+          element={<PageWrapper><AIAndML /></PageWrapper>}
+        />
+        <Route
+          path="/services/frontend-websites"
+          element={<PageWrapper><FrontendWeb /></PageWrapper>}
+        />
+
+        {/* Blog */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<Post />} />
       </Routes>
     </AnimatePresence>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,85 +1,33 @@
-// Main.js
 import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import Services from "./Services"; // Services main page
-import WebApp from "./WebApp"; // WebApp component
-import App from "./App";
-import MobileApps from "./MobileApp";
-import AIAndML from "./AISolution";
-import FrontendWeb from "./FrontendWeb";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
-import Post from "./Post";
-import Blog from "./Blog";
-import PageWrapper from "./PageWrapper";
 import AnimatedRoutes from "./AnimatedRoutes";
 
 // GA Listener component to track route changes
 function GAListener() {
   const location = useLocation();
+  
   useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", "G-3F63E7EG0D", { page_path: location.pathname });
+    // Ensure gtag is available and track page views
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag("config", "G-3F63E7EG0D", {
+        page_path: location.pathname + location.search,
+        page_title: document.title
+      });
+
+
     }
   }, [location]);
+
   return null;
 }
 
 export default function Main() {
   return (
     <Router>
-      <AnimatedRoutes />
       <ScrollToTop />
       <GAListener />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/services"
-          element={
-            <PageWrapper>
-              <Services />
-            </PageWrapper>
-          }
-        />
-        <Route
-          path="/services/web-apps"
-          element={
-            <PageWrapper>
-              <WebApp />
-            </PageWrapper>
-          }
-        />
-        <Route
-          path="/services/mobile-apps"
-          element={
-            <PageWrapper>
-              <MobileApps />
-            </PageWrapper>
-          }
-        />
-        <Route
-          path="/services/ai-solutions"
-          element={
-            <PageWrapper>
-              <AIAndML />
-            </PageWrapper>
-          }
-        />
-        <Route
-          path="/services/frontend-websites"
-          element={
-            <PageWrapper>
-              <FrontendWeb />
-            </PageWrapper>
-          }
-        />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<Post />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
